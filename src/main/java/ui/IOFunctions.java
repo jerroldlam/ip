1b462371faff4e3afb9f33d82b7e925119ae4d5f
jerroldlam <lamjerrold@gmail.com>
2020-08-15 14:38:40 +0800

    Update greeting messages with exceptions

--- src/main/java/ui/IOFunctions.java
+++ src/main/java/ui/IOFunctions.java
@@ -1,12 +1,14 @@
 package ui;
 
+import java.io.IOException;
+
 public class IOFunctions {
-    public static void greetEntry(){
+    public static void greetEntry() throws IOException {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
     }
 
-    public static void greetExit(){
+    public static void greetExit() throws IOException {
         System.out.println("Bye! Hope to see you again soon!");
     }
 }
