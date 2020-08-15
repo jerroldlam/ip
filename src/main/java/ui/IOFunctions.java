5017b49fb5c12d09c5a0008b5b135fd2d00440be
jerroldlam <lamjerrold@gmail.com>
2020-08-15 14:10:18 +0800

    Add greeting Messages
    -Create ui package
    -Add greeting function in iofunctions

---
+++ src/main/java/ui/IOFunctions.java
@@ -0,0 +1,12 @@
+package ui;
+
+public class IOFunctions {
+    public static void greetEntry(){
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+    }
+
+    public static void greetExit(){
+        System.out.println("Bye! Hope to see you again soon!");
+    }
+}
