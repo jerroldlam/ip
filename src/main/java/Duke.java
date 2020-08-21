fe8a4bdf3e3aa60282c5caa9933c8db27d8af967
jerroldlam <jerroldlam@github.com>
2020-08-21 14:13:43 +0800

    Add echo function into Duke Add ASCII Art for Duke
    Refactor UserGreeter

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
-import static ui.IOFunctions.*;
+import java.util.Scanner;
+import static ui.UserGreeter.*;
 
 public class Duke {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String line;
+
         System.out.println("-----------------------");
         greetEntry();
         System.out.println("-----------------------");
+        while (true) {
+            line = in.nextLine();
+            if (!line.equals("bye")) {
+                System.out.println(line);
+            } else {
+                break;
+            }
+        }
         greetExit();
         System.out.println("-----------------------");
     }
---
+++ src/main/java/ui/UserGreeter.java
@@ -0,0 +1,24 @@
+package ui;
+
+public class UserGreeter {
+
+    /*
+    ASCII ART from unknown author at https://pastebin.com/LPDM7dA0
+     */
+    public static void greetEntry() {
+        System.out.println("    　　∧_∧               Hello...");
+        System.out.println("      (  ･ω･)             You have woken me up...");
+        System.out.println("  　 ＿|　⊃／(＿＿_         What can I do for you?");
+        System.out.println("  ／　 └-(＿＿＿_／");
+        System.out.println("  ￣￣￣￣￣￣￣");
+    }
+
+    /*
+    ASCII ART from unknown author at https://pastebin.com/LPDM7dA0
+     */
+    public static void greetExit() {
+        System.out.println("    　 ＜⌒／ヽ-､_＿_       Bye.");
+        System.out.println("    ／＜_/＿＿＿＿／        I'm going back to sleep.");
+        System.out.println("    ￣￣￣￣￣￣￣");
+    }
+}
