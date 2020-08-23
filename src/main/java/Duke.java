6a88c8881993d0699363133cbb39a61544ed0934
jerroldlam <jerroldlam@github.com>
2020-08-23 17:30:53 +0800

    Refactor function task list into seperate folder

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,12 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 import static ui.UserGreeter.greetEntry;
 import static ui.UserGreeter.greetExit;
-import model.Task;
+import function.TaskList;
 
 public class Duke {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        ArrayList<Task> taskArrayList = new ArrayList<Task>();
+        TaskList userTaskList = new TaskList();
         String userInput;
         
         greetEntry();
@@ -18,30 +17,11 @@ public class Duke {
                 break;
 
             } else if (userInput.equals("list")) {
-                System.out.println("Here is your current task list!");
-                System.out.println("Hope you are on target!");
-                for (int listIndex = 1; listIndex <= taskArrayList.size(); listIndex++) {
-                    Task currentTask = taskArrayList.get(listIndex - 1);
-                    System.out.print(listIndex + ". ");
-
-                    if (currentTask.isTaskDone()) {
-                        System.out.print("[✓] ");
-                    } else {
-                        System.out.print("[✗] ");
-                    }
-                    System.out.println(currentTask.getTaskName());
-                }
+                userTaskList.printTaskList();
             } else if (userInput.contains("done")) {
-                String userInputNumber = userInput.substring(userInput.indexOf(" ") + 1);
-                int taskNumberCompleted = Integer.parseInt(userInputNumber);
-                taskArrayList.get(taskNumberCompleted - 1).setTaskDone(true);
-                System.out.println("Oh jolly! You finally completed this:");
-                System.out.println("  [✓] " + taskArrayList.get(taskNumberCompleted - 1).getTaskName());
+                userTaskList.setTaskComplete(userInput);
             } else {
-                Task newTask = new Task(userInput);
-                taskArrayList.add(newTask);
-                System.out.println("New task added: " + userInput);
-                System.out.println("I'll keep track of it for you!");
+                userTaskList.addTask(userInput);
             }
         }
         greetExit();
---
+++ src/main/java/function/TaskList.java
@@ -0,0 +1,63 @@
+package function;
+
+import model.Task;
+import java.util.ArrayList;
+
+public class TaskList {
+    private final ArrayList<Task> taskArrayList = new ArrayList<>();
+
+    public TaskList() { }
+
+    /**
+     * Prints current state of task list with the tasks and completeness.
+     * If task list is empty, it will inform the user of an empty task list.
+     */
+    public void printTaskList() {
+        if (taskArrayList.isEmpty()) {
+            System.out.println("You have no tasks yet!");
+            return;
+        } else {
+            System.out.println("Here is your current task list!");
+            System.out.println("Hope you are on target!");
+            for (int listIndex = 1; listIndex <= taskArrayList.size(); listIndex++) {
+                Task currentTask = taskArrayList.get(listIndex - 1);
+                System.out.print(listIndex + ". ");
+
+                if (currentTask.isTaskDone()) {
+                    System.out.print("[✓] ");
+                } else {
+                    System.out.print("[✗] ");
+                }
+                System.out.println(currentTask.getTaskName());
+            }
+        }
+    }
+
+    /**
+     * Takes a string of userInput and sets as name of a new task.
+     * Then, Adds the instance of the new task into taskArrayList.
+     *
+     * @param String userInput name of new task.
+     */
+    public void addTask(String userInput) {
+        Task newTask = new Task(userInput);
+        taskArrayList.add(newTask);
+        System.out.println("New task added: " + userInput);
+        System.out.println("I'll keep track of it for you!");
+    }
+
+    /**
+     * Takes a string of userInput in the format of "done X", where X is an integer marking the task index
+     * of the task that the user wants to indicate as done. Find the corresponding index from int X and
+     * sets the task's boolean isDone to be true.
+     *
+     * @param String userInput user's input in the format "done x"
+     */
+    public void setTaskComplete(String userInput) {
+        String userInputNumber = userInput.substring(userInput.indexOf(" ") + 1);
+        int taskNumberCompleted = Integer.parseInt(userInputNumber);
+        taskArrayList.get(taskNumberCompleted - 1).setTaskDone(true);
+        System.out.println("Oh jolly! You finally completed this:");
+        System.out.println("  [✓] " + taskArrayList.get(taskNumberCompleted - 1).getTaskName());
+    }
+}
